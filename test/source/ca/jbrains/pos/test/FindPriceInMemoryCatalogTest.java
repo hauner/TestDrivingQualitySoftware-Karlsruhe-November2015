1a936527cab4b79a0b29e52d6c5c4898e9f9a14a
J. B. Rainsberger <me@jbrains.ca>
2015-11-25 10:41:08 +0100

    We can now find the right price, even when there are others around it.

--- test/source/ca/jbrains/pos/test/FindPriceInMemoryCatalogTest.java
+++ test/source/ca/jbrains/pos/test/FindPriceInMemoryCatalogTest.java
@@ -5,6 +5,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 public class FindPriceInMemoryCatalogTest {
@@ -13,6 +14,21 @@ public class FindPriceInMemoryCatalogTest {
         final Price matchingPrice = Price.cents(1250);
         final InMemoryCatalog catalog = new InMemoryCatalog(
                 Collections.singletonMap("12345", matchingPrice));
+
+        Assert.assertEquals(matchingPrice, catalog.findPrice("12345"));
+    }
+
+    @Test
+    public void productFoundAmongOthers() throws Exception {
+        final Price matchingPrice = Price.cents(1250);
+
+        final InMemoryCatalog catalog = new InMemoryCatalog(
+                new HashMap() {{
+                    put("23456", Price.cents(799));
+                    put("12345", matchingPrice);
+                    put("34567", Price.cents(905));
+                }}
+        );
 
         Assert.assertEquals(matchingPrice, catalog.findPrice("12345"));
     }
